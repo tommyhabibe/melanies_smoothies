8cc7ef9c4c2e624536f2a80a02d9d0848222f6ef
tommyhabibe <tthabibe@habibe.com>
2024-07-05 19:34:01 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,8 @@ import streamlit as st
 from snowflake.snowpark.functions import col
 import requests
 
+st.set_page_config(layout='wide')
+
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
 st.write(
