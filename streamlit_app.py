9e2679666722284fa10a48c4b04e9801e2ad2b86
tommyhabibe <tthabibe@habibe.com>
2024-07-05 19:30:33 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,4 +45,5 @@ if ingredient_list:
         
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response)
+st.text(fruityvice_response.json())
+
